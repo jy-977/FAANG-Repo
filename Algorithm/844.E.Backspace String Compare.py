2bd871cd11059a09e8700148864dcad8fb6e785e
Jeongyun Lee <suejlemo@gmail.com>
2022-08-20 23:46:17 +0200

    20/AUG/2022 844

---
+++ Algorithm/844.E.Backspace String Compare.py
@@ -0,0 +1,11 @@
+from collections import deque
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        #problem analysis
+        
+        #1) time : O(n) ==> binary search O(logn) , twopointers
+        #2) space : O(1) ===> stack X
+        #2) short range 
+        
+        
+        #
