1683c815c4c96a6023cf528c2e62bdbb61b125f4
Jeongyun Lee <suejlemo@gmail.com>
2022-09-21 23:48:05 +0200

    facebook interview prep

--- Temp/new_temp.py
+++ Temp/new_temp.py
@@ -1,40 +1,46 @@
-Sep 10
+# Sep 10
 
-# 무엇인가 내 능력보다 약간 위의 것을 도전할때는 자신감을 잃지 않는 것이 중요하다
-# 스스로의 능력과 자질을 의심하는 시기가 매일 찾아 오겠지만 지금의 현상을 유지해야한다
+# # 무엇인가 내 능력보다 약간 위의 것을 도전할때는 자신감을 잃지 않는 것이 중요하다
+# # 스스로의 능력과 자질을 의심하는 시기가 매일 찾아 오겠지만 지금의 현상을 유지해야한다
 
-# 누가 그랬는데 사람들은 열심히 살고, 열심히 하지 않는다고 한다.
-# 심지어는 그냥 하지 않는다
-# 그래서 중요한것은 열심히 하는 것보다 그냥 하는 것이다. 
-# 성인이 되어서는 도전과 발전 그리고 공부를 하지 않는 사람이 거의 대부분이라서
-# 그냥 그 중 하나를 하기만 하면 성취를 이룰 수 있다고 한다
+# # 누가 그랬는데 사람들은 열심히 살고, 열심히 하지 않는다고 한다.
+# # 심지어는 그냥 하지 않는다
+# # 그래서 중요한것은 열심히 하는 것보다 그냥 하는 것이다. 
+# # 성인이 되어서는 도전과 발전 그리고 공부를 하지 않는 사람이 거의 대부분이라서
+# # 그냥 그 중 하나를 하기만 하면 성취를 이룰 수 있다고 한다
 
-Sep 13
-전략을 잘 세워야할것 같다. 
-leet code난이도는 생각보다 중요하지 않고 일단 cv던져서 면접까지 가보는게 중요하다
-이번주까지 cv준비해서 던져야지
+# Sep 13
+# 전략을 잘 세워야할것 같다. 
+# leet code난이도는 생각보다 중요하지 않고 일단 cv던져서 면접까지 가보는게 중요하다
+# 이번주까지 cv준비해서 던져야지
 
-Four-year full-tuition scholarship awarded for academic excellence in high school.
-Master's degree in Informatics Engineering
-<work experience>
-Teacher assistance ; 
-Practical teaching on big data course of up to 45 students : R programming
+# Four-year full-tuition scholarship awarded for academic excellence in high school.
+# Master's degree in Informatics Engineering
+# <work experience>
+# Teacher assistance ; 
+# Practical teaching on big data course of up to 45 students : R programming
 
-Communication skill in canal 
-: lead the communications and analysis the requirements with billing manager  for designing and improving user experience
+# Communication skill in canal 
+# : lead the communications and analysis the requirements with billing manager  for designing and improving user experience
 
-AUTOIN 
-
-
-
-첫번째줄 업무 설명
-1.	First line : general work description
-2.	Specific work experience 1 + Achievement 1
-3.	Specific work experience 2 + Achievement 2
+# AUTOIN 
 
 
-Visualization + short time face to face meeting
 
-Questions – accurate requirements
-www.linkedin.com/in/jeongyun--lee
+# 첫번째줄 업무 설명
+# 1.	First line : general work description
+# 2.	Specific work experience 1 + Achievement 1
+# 3.	Specific work experience 2 + Achievement 2
 
+
+# Visualization + short time face to face meeting
+
+# Questions – accurate requirements
+# www.linkedin.com/in/jeongyun--lee
+
+
+# 퇴사 얼마 안남았는데,, 
+# 비자부터 코테, 인터뷰까지 너무 무섭다
+# faang , ms 이런거 떨어지면 재지원까지 1년걸린다는데 그냥 이번에 한번에 못붙으면 바로 한국 가야하는거 아닌가..
+# 시간이 없는데 걱정되고 불안하다
+# 
